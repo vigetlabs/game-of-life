054323e0f4e0f7a5a5d5d47e183fc054fdeed7fc
h0tl33t <h0tl33t@gmail.com>
2015-07-28 11:00:32 -0400

    Implement ticking

--- src/board.js
+++ src/board.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
+const ALIVE = 1
+const DEAD  = 0
+const SPEED = 1000
 const Board = React.createClass({
   getInitialState() {
     return {
-      size: 20,
-      rows: this.buildRows(20)
+      size:    20,
+      rows:    this.buildRows(20),
+      ticking: false
     }
+  },
+
+  componentDidMount() {
+    this.start()
   },
 
   buildRows(size) {
@@ -37,6 +45,57 @@ const Board = React.createClass({
     return (
       <li className={ classes } key={ `${x}/${y}` } />
     )
+  },
+
+  tick() {
+    if (!this.state.ticking) return
+
+    console.log("Tick!")
+
+    let rows = []
+
+    for (let y = 0; y < this.state.size; y++) {
+      let row = []
+
+      for (let x = 0; x < this.state.size; x++) {
+        let cell      = { x: x, y: y, alive: this.state.rows[y][x] }
+        let liveCount = this.liveNeighborCount.call(this, cell)
+
+        if (liveCount === 3) {
+          row.push(ALIVE)
+        } else if (liveCount === 2) {
+          cell.alive ? row.push(ALIVE) : row.push(DEAD)
+        } else {
+          row.push(DEAD)
+        }
+      }
+
+      rows.push(row)
+    }
+
+    this.setState({ rows: rows })
+  },
+
+  liveNeighborCount(cell) {
+    let { rows, size } = this.state
+    let count          = 0
+
+    count += (rows[cell.y]     || [])[cell.x - 1] || 0
+    count += (rows[cell.y]     || [])[cell.x + 1] || 0
+    count += (rows[cell.y - 1] || [])[cell.x]     || 0
+    count += (rows[cell.y - 1] || [])[cell.x - 1] || 0
+    count += (rows[cell.y - 1] || [])[cell.x + 1] || 0
+    count += (rows[cell.y + 1] || [])[cell.x]     || 0
+    count += (rows[cell.y + 1] || [])[cell.x - 1] || 0
+    count += (rows[cell.y + 1] || [])[cell.x + 1] || 0
+
+    return count
+  },
+
+  start() {
+    this.setState({ ticking: true })
+
+    setInterval(() => { this.tick.call(this) }, SPEED)
   },
 
   render() {
